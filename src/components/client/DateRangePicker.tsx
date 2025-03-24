9f2377a9b40f58f3992341f3e3df3726b7c5c176
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2025-03-24 11:27:32 +0200

    minor clean up

--- src/components/client/DateRangePicker.tsx
+++ src/components/client/DateRangePicker.tsx
@@ -4,15 +4,12 @@ import React, { useState } from "react";
 import DatePicker, { DateObject } from "react-multi-date-picker";
 
 export const DateRangePicker = () => {
-  // In range mode, we expect value to be an array of Date objects.
   const [value, setValue] = useState<Date[]>([]);
 
   const handleSetValue = (date: DateObject | DateObject[] | null) => {
     if (Array.isArray(date)) {
-      // Filter out any nullish values and convert to native Date objects.
       setValue(date.filter(Boolean).map((d) => d.toDate()));
     } else {
-      // If a single date is selected, wrap it in an array; if null, set an empty array.
       setValue(date ? [date.toDate()] : []);
     }
   };
