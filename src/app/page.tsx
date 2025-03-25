1f34a3d774edd2da58adb716c9a84d15e974098a
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2025-03-25 17:20:56 +0200

    renamed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { TableClient } from "@/components/client/TableClient";
+import { DataExplorerClient } from "@/components/client/DataExplorerClient";
 
 export default async function DataExplorerPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">TULU Data Explorer</h1>
-      <TableClient />
+      <DataExplorerClient />
     </div>
   );
 }
