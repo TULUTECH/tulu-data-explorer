1404025dc686ff4f561bce28c5978d3a03fbc71f
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2025-03-26 14:46:50 +0200

    removed unused import

--- src/components/client/Columns.tsx
+++ src/components/client/Columns.tsx
@@ -1,11 +1,7 @@
 "use client";
-import rawDataJson from "@/data/normalized_omp_data.json";
 import { ITypeParsedOmpData } from "@/types/data";
-import { parseOmpDataTypes } from "@/utils/parseNormalizedOmpData";
 import { createColumnHelper, FilterFn } from "@tanstack/react-table";
 import { format } from "date-fns";
-
-export const omProptechData = parseOmpDataTypes(rawDataJson);
 
 export const columnHelper = createColumnHelper<ITypeParsedOmpData>();
 
