cec129c82385e75e010751542b27dc8e261365de
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2025-03-27 15:12:35 +0200

    custom hook for table config

---
+++ src/hooks/useTableConfiguration.ts
@@ -0,0 +1,69 @@
+import {
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel,
+  getGroupedRowModel,
+  GroupingState,
+  getSortedRowModel,
+  VisibilityState,
+  Updater,
+  OnChangeFn,
+} from "@tanstack/react-table";
+import { Dimension, ITypeParsedOmpData } from "@/types/data";
+import { columns } from "@/components/client/Columns";
+import { setSelectedDimensions } from "@/store/slices/dataExplorerSlice";
+import { AppDispatch } from "@/store/store";
+
+const INITIAL_TABLE_STATE = {
+  pagination: {
+    pageIndex: 0,
+    pageSize: 100,
+  },
+  grouping: [],
+};
+
+interface TableConfigurationProps {
+  tableData: ITypeParsedOmpData[];
+  appliedGrouping: GroupingState;
+  columnVisibility: VisibilityState;
+  // TODO: correct type
+  dispatch: AppDispatch; // You might want to type this properly with your Redux dispatch type
+  isMountedRef: React.RefObject<boolean>;
+  setColumnVisibility: (visibility: VisibilityState) => void;
+}
+
+export const useTableConfiguration = ({
+  tableData,
+  appliedGrouping,
+  columnVisibility,
+  dispatch,
+  isMountedRef,
+  setColumnVisibility,
+}: TableConfigurationProps) => {
+  const handleColumnVisibilityChange: OnChangeFn<VisibilityState> = (updaterOrValue: Updater<VisibilityState>) => {
+    if (typeof updaterOrValue === "function") {
+      setColumnVisibility(updaterOrValue(columnVisibility));
+    } else {
+      setColumnVisibility(updaterOrValue);
+    }
+  };
+  return useReactTable({
+    data: tableData,
+    columns,
+    state: { grouping: appliedGrouping, columnVisibility },
+    onGroupingChange: (updatedGrouping) => {
+      if (isMountedRef.current) {
+        dispatch(setSelectedDimensions(updatedGrouping as Dimension[]));
+      }
+    },
+    onColumnVisibilityChange: handleColumnVisibilityChange,
+    // Table feature enablers
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getGroupedRowModel: getGroupedRowModel(),
+    initialState: INITIAL_TABLE_STATE,
+  });
+};
